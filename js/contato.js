0b4e223aeeee826b1f8c94836632b44cb6f7a392
CavichioliFIAP <Rm551100@fiap.com.br>
2023-05-30 14:51:50 -0300

    validando campos do js

--- js/contato.js
+++ js/contato.js
@@ -151,96 +151,141 @@ function validateInputTelefone() {
     }
   }
 
-const inputCPF = document.querySelector("#cpf");
-inputCPF.addEventListener("focus", ()=>{
-    inputCPF.setAttribute("style","outline-color: #ff0000;");
-})
-
-inputCPF.addEventListener("keyup", ()=>{
-
-    const lblcpf = document.querySelector("label[for='cpf']");
-    if(inputCPF.value.length < 11){
-        inputCPF.setAttribute("style","outline-color: #ff0000;");
-        lblcpf.setAttribute("style","color: #ff0000;");
-    }else{
-        inputCPF.setAttribute("style","outline-color: #00ff00;");
-        lblcpf.setAttribute("style","color: #00ff00;");
+  const inputCPF = document.querySelector("#cpf");
+  inputCPF.addEventListener("focus", ()=>{
+      inputCPF.setAttribute("style","outline-color: #ff0000;");
+  })
+  
+  inputCPF.addEventListener("keyup", ()=>{
+  
+      const lblcpf = document.querySelector("label[for='cpf']");
+      if(inputCPF.value.length < 11){
+          inputCPF.setAttribute("style","outline-color: #ff0000;");
+          lblcpf.setAttribute("style","color: #ff0000;");
+      }else{
+          inputCPF.setAttribute("style","outline-color: #00ff00;");
+          lblcpf.setAttribute("style","color: #00ff00;");
+      }
+  })
+  
+  function validateInputcpf() {
+      const input = document.getElementById('cpf').value;
+      const errorMsg = document.getElementById('errorMsgcpf');
+  
+      if (input.length < 11) {
+        errorMsg.innerText = 'O campo "CPF" deve ter mais de 11 dígitos.';
+        errorMsg.style.display = 'block';
+      } else {
+        errorMsg.style.display = 'none';
+      }
     }
-})
-
-
-const inputDatanasc = document.querySelector("#datanasc");
-inputDatanasc.addEventListener("focus", ()=>{
-    inputDatanasc.setAttribute("style","outline-color: #ff0000;");
-})
-
-inputDatanasc.addEventListener("keyup", ()=>{
-
-    const lbldatanasc = document.querySelector("label[for='datanasc']");
-    if(inputDatanasc.value.length < 10){
-        inputDatanasc.setAttribute("style","outline-color: #ff0000;");
-        lbldatanasc.setAttribute("style","color: #ff0000;");
-    }else{
-        inputDatanasc.setAttribute("style","outline-color: #00ff00;");
-        lbldatanasc.setAttribute("style","color: #00ff00;");
+  
+  
+  const inputDatanasc = document.querySelector("#datanasc");
+  inputDatanasc.addEventListener("focus", ()=>{
+      inputDatanasc.setAttribute("style","outline-color: #ff0000;");
+  })
+  
+  inputDatanasc.addEventListener("keyup", ()=>{
+  
+      const lbldatanasc = document.querySelector("label[for='datanasc']");
+      if(inputDatanasc.value.length < 10){
+          inputDatanasc.setAttribute("style","outline-color: #ff0000;");
+          lbldatanasc.setAttribute("style","color: #ff0000;");
+      }else{
+          inputDatanasc.setAttribute("style","outline-color: #00ff00;");
+          lbldatanasc.setAttribute("style","color: #00ff00;");
+      }
+  })
+  
+  function validateInputdatanasc() {
+      const input = document.getElementById('datanasc').value;
+      const errorMsg = document.getElementById('errorMsgdatanasc');
+  
+      if (input.length < 10) {
+        errorMsg.innerText = 'Insira uma Data de Nascimento válida.';
+        errorMsg.style.display = 'block';
+      } else {
+        errorMsg.style.display = 'none';
+      }
     }
-})
-
-const inputGenero = document.querySelector("#genero");
-inputGenero.addEventListener("focus", ()=>{
-    inputGenero.setAttribute("style","outline-color: #ff0000;");
-})
-
-inputGenero.addEventListener("keyup", ()=>{
-
-    const lblGenero = document.querySelector("label[for='genero']");
-    if(inputGenero.value.length < 2){
-        inputGenero.setAttribute("style","outline-color: #ff0000;");
-        lblGenero.setAttribute("style","color: #ff0000;");
-    }else{
-        inputGenero.setAttribute("style","outline-color: #00ff00;");
-        lblGenero.setAttribute("style","color: #00ff00;");
+  
+  
+  const inputGenero = document.querySelector("#genero");
+  inputGenero.addEventListener("focus", ()=>{
+      inputGenero.setAttribute("style","outline-color: #ff0000;");
+  })
+  
+  inputGenero.addEventListener("keyup", ()=>{
+  
+      const lblGenero = document.querySelector("label[for='genero']");
+      if(inputGenero.value.length < 2){
+          inputGenero.setAttribute("style","outline-color: #ff0000;");
+          lblGenero.setAttribute("style","color: #ff0000;");
+      }else{
+          inputGenero.setAttribute("style","outline-color: #00ff00;");
+          lblGenero.setAttribute("style","color: #00ff00;");
+      }
+  })
+  
+  const inputCep = document.querySelector("#cep");
+  inputCep.addEventListener("focus", ()=>{
+      inputCep.setAttribute("style","outline-color: #ff0000;");
+  })
+  
+  inputCep.addEventListener("keyup", ()=>{
+  
+      const lblCep = document.querySelector("label[for='cep']");
+      if(inputCep.value.length < 8){
+          inputCep.setAttribute("style","outline-color: #ff0000;");
+          lblCep.setAttribute("style","color: #ff0000;");
+      }else{
+          inputCep.setAttribute("style","outline-color: #00ff00;");
+          lblCep.setAttribute("style","color: #00ff00;");
+      }
+  })
+  
+  function validateInputcep() {
+      const input = document.getElementById('cep').value;
+      const errorMsg = document.getElementById('errorMsgcep');
+  
+      if (input.length < 8) {
+        errorMsg.innerText = 'Insira um CEP válido.';
+        errorMsg.style.display = 'block';
+      } else {
+        errorMsg.style.display = 'none';
+      }
     }
-})
-
-const inputCep = document.querySelector("#cep");
-inputCep.addEventListener("focus", ()=>{
-    inputCep.setAttribute("style","outline-color: #ff0000;");
-})
-
-inputCep.addEventListener("keyup", ()=>{
-
-    const lblCep = document.querySelector("label[for='cep']");
-    if(inputCep.value.length < 8){
-        inputCep.setAttribute("style","outline-color: #ff0000;");
-        lblCep.setAttribute("style","color: #ff0000;");
-    }else{
-        inputCep.setAttribute("style","outline-color: #00ff00;");
-        lblCep.setAttribute("style","color: #00ff00;");
+  
+  const inputEndereco = document.querySelector("#endereco");
+  inputEndereco.addEventListener("focus", ()=>{
+      inputEndereco.setAttribute("style","outline-color: #ff0000;");
+  })
+  
+  inputEndereco.addEventListener("keyup", ()=>{
+  
+      const lblEndereco = document.querySelector("label[for='endereco']");
+      if(inputEndereco.value.length < 5){
+          inputEndereco.setAttribute("style","outline-color: #ff0000;");
+          lblEndereco.setAttribute("style","color: #ff0000;");
+      }else{
+          inputEndereco.setAttribute("style","outline-color: #00ff00;");
+          lblEndereco.setAttribute("style","color: #00ff00;");
+      }
+  })
+  
+  function validateInputendereco() {
+      const input = document.getElementById('endereco').value;
+      const errorMsg = document.getElementById('errorMsgendereco');
+  
+      if (input.length < 5) {
+        errorMsg.innerText = 'Insira um endereço válido';
+        errorMsg.style.display = 'block';
+      } else {
+        errorMsg.style.display = 'none';
+      }
     }
-})
-
-const inputEndereco = document.querySelector("#endereco");
-inputEndereco.addEventListener("focus", ()=>{
-    inputEndereco.setAttribute("style","outline-color: #ff0000;");
-})
-
-inputEndereco.addEventListener("keyup", ()=>{
-
-    const lblEndereco = document.querySelector("label[for='endereco']");
-    if(inputEndereco.value.length < 5){
-        inputEndereco.setAttribute("style","outline-color: #ff0000;");
-        lblEndereco.setAttribute("style","color: #ff0000;");
-    }else{
-        inputEndereco.setAttribute("style","outline-color: #00ff00;");
-        lblEndereco.setAttribute("style","color: #00ff00;");
-    }
-})
-
-
-
-
-
-
-
-
+  
+  
+  
+  
